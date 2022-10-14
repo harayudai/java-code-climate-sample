a0e07161cc40d3dbbdd8d2cb997c4ff16157c98d
harayudai <harayuh@nttdata-ccs.co.jp>
2022-10-14 12:46:23 +0900

    add invalid java file

---
+++ src/main/java/com/codecy/sample/utility/Test.java
@@ -0,0 +1,23 @@
+package com.codecy.sample.utility;
+
+import java.util.*;
+import java.time.*;
+
+public class Test {
+
+    // AvoidUsingHardCodedIP:OFF
+    private String ip = "127.0.0.1";
+
+    public void test(String name) {
+        String unUsed = "aaa";
+        try {
+            // AvoidReassigningParameters:ON
+            name = name.trim();
+            System.out.println("Hello " + name);
+            System.out.println(ip);
+        } catch (Exception e) {
+            // AvoidPrintStackTrace:OFF
+            e.printStackTrace();
+        }
+    }
+}
